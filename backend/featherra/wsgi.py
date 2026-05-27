a005db7b17adcff150241335bf97a3f2859012da
Jerome Maunga <jerhomemaunga@gmail.com>
2026-05-27 17:53:22 +0300

    Updates

--- backend/featherra/wsgi.py
+++ backend/featherra/wsgi.py
@@ -1,6 +1,10 @@
 import os
 
+import pymysql
 from django.core.wsgi import get_wsgi_application
+
+pymysql.version_info = (1, 4, 6, "final", 0)
+pymysql.install_as_MySQLdb()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'featherra.settings')
 
