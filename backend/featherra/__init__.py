ce65769c1e38b1177a25801736d04094eb86a1d2
Jerome Maunga <jerhomemaunga@gmail.com>
2026-05-27 17:51:44 +0300

    Updated the pymysql.version

--- backend/featherra/__init__.py
+++ backend/featherra/__init__.py
@@ -1,1 +1,6 @@
 # Featherra core configuration package.
+import pymysql
+
+# Django's MySQL backend checks MySQLdb.version_info; patch PyMySQL to match.
+pymysql.version_info = (1, 4, 6, "final", 0)
+pymysql.install_as_MySQLdb()
